9215ac7665251ed1b1d6878f2b33486f0940343e
Matthew <matthew6303@gmail.com>
2022-01-08 15:57:15 +0800

    feat: image skeleton & project inner modal

--- src/components/molecules/ProjectCard/index.js
+++ src/components/molecules/ProjectCard/index.js
@@ -5,12 +5,14 @@ import { projectImageRef } from 'services/firebase';
 
 import styles from './styles.module.scss';
 
-const ProjectCard = ({ className, title = '', thumbnail, onClick = () => {} }) => {
+const ProjectCard = ({ className, title = '', thumbnail = '', tag = [], onClick = () => {} }) => {
 	const [imageUrl, setImageUrl] = useState('');
 
 	async function getImageUrl() {
 		const url = await projectImageRef(thumbnail);
-		setImageUrl(url);
+		const img = new Image();
+		img.src = url;
+		img.onload = () => setImageUrl(url);
 	}
 
 	useEffect(() => {
@@ -22,11 +24,16 @@ const ProjectCard = ({ className, title = '', thumbnail, onClick = () => {} }) =
 			role="button"
 			tabIndex={0}
 			onKeyPress={() => {}}
-			className={classnames(styles.container, className)}
+			className={classnames(styles.container, imageUrl === '' && 'loading-skeleton', className)}
 			onClick={onClick}
-			style={{ backgroundImage: `url(${imageUrl})` }}
+			style={imageUrl !== '' ? { backgroundImage: `url(${imageUrl})` } : {}}
 		>
 			<h2>{title}</h2>
+			<div className={styles.tagList}>
+				{tag.map(_tag => (
+					<p key={_tag}>{_tag}</p>
+				))}
+			</div>
 		</div>
 	);
 };
--- src/components/organisms/Project/index.js
+++ src/components/organisms/Project/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useTransition, useSpringRef, animated } from 'react-spring';
+import { use100vh } from 'react-div-100vh';
 
 import { useModal } from 'models/modal';
 import { useProject } from 'models/project';
@@ -7,6 +8,8 @@ import { useProject } from 'models/project';
 import SectionTitle from 'components/atoms/SectionTitle';
 import ProjectCard from 'components/molecules/ProjectCard';
 import Modal from 'components/molecules/Modal';
+
+import { ReactComponent as Close } from 'images/icon/close.svg';
 
 import styles from './styles.module.scss';
 
@@ -30,12 +33,13 @@ const Section = ({ start, data, sectionTitle, subTitle }) => {
 				<p>{subTitle}</p>
 			</SectionTitle>
 			<div className={styles.content} style={{ minHeight: 136 * Math.ceil(data.length / 2) }}>
-				{transitions((style, { title, thumbnail }) => (
+				{transitions((style, { title, thumbnail, tag }) => (
 					<animated.div className={styles.animated} style={style}>
 						<ProjectCard
 							key={title}
 							title={title}
 							thumbnail={thumbnail}
+							tag={tag}
 							onClick={() => openModal({ type: 'project' })}
 						/>
 					</animated.div>
@@ -46,6 +50,7 @@ const Section = ({ start, data, sectionTitle, subTitle }) => {
 };
 
 const Project = ({ hitFlag }) => {
+	const height = use100vh();
 	const [{ type }, { closeModal }] = useModal();
 	const [{ projectList }, { fetchProjects }] = useProject();
 
@@ -82,9 +87,12 @@ const Project = ({ hitFlag }) => {
 				/>
 			</div>
 			<Modal active={type === 'project'}>
-				<button type="button" onClick={() => closeModal()}>
-					close
-				</button>
+				<div className={styles.modalWrapper} style={{ minHeight: 0.9 * height, top: 0.1 * height }}>
+					<div className={styles.modalContainer}>
+						<Close onClick={closeModal} />
+						<h1>Title</h1>
+					</div>
+				</div>
 			</Modal>
 		</div>
 	);
